aa9fbc589e30e9c5c1fd5ae7e3923db31a75ed3b
brayan-cincoe <brayanrcaicedo@gmail.com>
2021-12-21 01:47:20 -0500

    Remove some print commands

--- Lab1/p1_ramos/adc.c
+++ Lab1/p1_ramos/adc.c
@@ -35,10 +35,13 @@ void adc_setup()
     adc_fifo_setup(
         true,
         false,
-        1,
+        2,
         false,
         false);
-
+    
+    irq_set_exclusive_handler(ADC_IRQ_FIFO, adc_handler);
+    adc_irq_set_enabled(true);
+    irq_set_enabled(ADC_IRQ_FIFO, true);
 }
 
 // Capturing data
@@ -47,8 +50,4 @@ void adc_capture(uint8_t adc_num)
     adc_fifo_drain();
     adc_select_input(adc_num);
     adc_run(true);
-    
-    irq_set_exclusive_handler(ADC_IRQ_FIFO, adc_handler);
-    adc_irq_set_enabled(true);
-    irq_set_enabled(ADC_IRQ_FIFO, true);
 }
