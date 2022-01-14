3bc2e309d362f486904025fff4a68736e84f446e
brayan-cincoe <brayanrcaicedo@gmail.com>
2022-01-13 23:04:15 -0500

    Define row and column ports

---
+++ Lab2/p2_ramos/kbi.h
@@ -0,0 +1,14 @@
+#ifndef _KBI_H
+#define _KBI_H
+
+// 4x4 keyboard
+#define ROW1 1
+#define ROW2 2
+#define ROW3 3
+#define ROW4 4
+#define COL1 5
+#define COL2 6
+#define COL3 7
+#define COL4 8
+
+#endif
