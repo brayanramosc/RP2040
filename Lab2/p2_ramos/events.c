82f45f5d2c45f58da79c81442a15a808e9e60540
brayan-cincoe <brayanrcaicedo@gmail.com>
2022-01-25 20:56:31 -0500

    Change date format

--- Lab2/p2_ramos/events.c
+++ Lab2/p2_ramos/events.c
@@ -30,6 +30,13 @@ void events_controller(void) {
 						printf("Tecla: %c\n", key);
 						print_current_time();
 					#endif
+					#if RUN_MODE == DEBUG
+						if (key == 'A') {
+							printf("Tecla: %c\n", key);
+							print_date_and_time();
+						}
+						
+					#endif
 				}
 			}
 		}
